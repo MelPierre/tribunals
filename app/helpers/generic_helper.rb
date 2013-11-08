2674f6ad82ba0765dd987f0af16875a9728366fa
Aleksandar Simic <alex.simic@digital.justice.gov.uk>
2013-11-08 16:35:13 +0000

    Removed EAT & FTT helper & moved to GenericHelper
    Moved display_parties helper to GenericHelper
    
    Handling the missing (sub)categories in helpers
    
    Streamlined the display_parties helper
    
    Fix generic helper
    
    Remove redundant whitespace
    
    Simplify join display strings helper
    
    Set default separator for display helper. Allow override from view.

--- app/helpers/generic_helper.rb
+++ app/helpers/generic_helper.rb
@@ -1,25 +1,26 @@
 module GenericHelper
-  def display_categories(decision)
+  def display_categories(decision, separator = " / ")
     prefix = decision.class.name.split(/(?=[A-Z])/)[0].downcase
     categories = []
 
     decision.send("#{prefix}_subcategories").each do |subcat|
+      category = begin
+                   subcat.send("#{prefix}_category").name
+                 rescue NoMethodError
+                   nil
+                 end
 
-      begin
-        category = subcat.send("#{prefix}_category").name
-      rescue NoMethodError
-        category = ''
-      end
-
-      subcategory = (subcat.name || '')
-
-      unless category.blank?
-        categories << "#{category}/#{subcategory}"
-      else
-        categories << subcat.name
-      end
+      categories << join_display_strings([category, subcat.name], separator)
     end
 
     categories.join(', ')
+  end
+
+  def display_parties(decision, separator = " vs ")
+    join_display_strings([decision.claimant, decision.respondent], separator)
+  end
+
+  def join_display_strings(strings_array, separator)
+    strings_array.reject(&:blank?).join(separator)
   end
 end
