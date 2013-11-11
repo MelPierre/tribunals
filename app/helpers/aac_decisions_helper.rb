e69463525278351c68ca0244250ac55aa1aca3a6
Hrishi Mittal <hrishimittal@gmail.com>
2013-11-11 18:04:30 +0000

    AAC filter by subcategories

--- app/helpers/aac_decisions_helper.rb
+++ app/helpers/aac_decisions_helper.rb
@@ -1,14 +1,5 @@
 module AacDecisionsHelper
   #TODO: Add page_title helper which does not conflict with other helpers
-  def category_name(aac_decision)
-    if aac_decision.aac_subcategory && aac_decision.aac_subcategory.aac_category
-        aac_decision.aac_subcategory.aac_category.name
-    end
-  end
-
-  def subcategory_name(aac_decision)
-    aac_decision.aac_subcategory.name if aac_decision.aac_subcategory
-  end
 
   def judge_names(aac_decision)
     aac_decision.judges.pluck(:name).join(", ")
