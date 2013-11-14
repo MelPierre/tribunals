43fdf28020bc5e15c2cc5ba209d115bc8656f22f
Aleksandar Simić <alex.simic@digital.justice.gov.uk>
2013-11-14 15:34:55 +0000

    Added ordered_by to FTT
    FTT Remove redundant search method. Add shared sort filter partial.
    
    Refactor to use shared sort filter.

--- app/controllers/ftt_decisions_controller.rb
+++ app/controllers/ftt_decisions_controller.rb
@@ -2,16 +2,17 @@ class FttDecisionsController < ApplicationController
   before_filter :enable_varnish
 
   def index
-    set_cache_control(FttDecision.maximum(:updated_at))    
-    @order_by = 'hearing_date'
+    set_cache_control(FttDecision.maximum(:updated_at))
+    @order_by = 'decision_date'
+    @sort_options = [["Date of decision", "decision_date"], ["Date of update", "last_updatedtime"]]
     params[:search] ||= {}
-    if params[:search].present?  
+    if params[:search].present?
        @order_by = params[:search][:sort] || @order_by
     end
     @date_column_title = (@order_by == 'decision_date') ? 'Date of decision' : 'Date of update'
     @categories_title = 'Categories: '
     @decisions = FttDecision.ordered.paginate(:page => params[:page], :per_page => 30)
-    @decisions = @decisions.filtered(params[:search]) if params[:search].present?    
+    @decisions = @decisions.filtered(params[:search]) if params[:search].present?
   end
 
   def show
