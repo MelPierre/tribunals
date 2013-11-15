c899fbee2f1aa1d8441c7c5d159c98d18fbaf7ad
Hrishi Mittal <hrishimittal@gmail.com>
2013-11-15 13:23:49 +0000

    Remove old index migrations

--- db/migrate/20131007113059_redo_fts_index.rb
+++
@@ -1,8 +0,0 @@
-class RedoFtsIndex < ActiveRecord::Migration
-  disable_ddl_transaction!
-
-  def change
-    execute "DROP INDEX decisions_fts_idx"
-    execute "CREATE INDEX CONCURRENTLY decisions_fts_idx ON decisions USING gin(to_tsvector('english', text))"
-  end
-end
