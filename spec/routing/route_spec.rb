895281456565667a00e175f404568e1234dc4954
Aleksandar Simić <alex.simic@digital.justice.gov.uk>
2013-11-19 14:13:37 +0000

    Changed /employment to /eat as per TRIBS-200

--- spec/routing/route_spec.rb
+++ spec/routing/route_spec.rb
@@ -14,10 +14,10 @@ describe Tribunals::Application.routes do
   end
 
   it "should render EAT's index routes" do
-    expect(get: "/employment").to route_to(controller: 'eat_decisions', action: 'index')
+    expect(get: "/eat").to route_to(controller: 'eat_decisions', action: 'index')
   end
 
   it "should render EAT's show routes" do
-    expect(get: "/employment/1").to route_to(controller: 'eat_decisions', action: 'show', id: '1')
+    expect(get: "/eat/1").to route_to(controller: 'eat_decisions', action: 'show', id: '1')
   end
 end
