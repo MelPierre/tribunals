d0fa7feda9984f21bdecb64b63850eee508fe5ca
Hrishi Mittal <hrishimittal@gmail.com>
2014-03-18 11:37:34 +0000

    Change urls
    Change urls
    
    Fix route specs, add one more.
    
    Change admin urls for UTIAC
    
    Add Changelog entry
    
    Remove structure.sql

--- config/routes.rb
+++ config/routes.rb
@@ -1,33 +1,39 @@
 Tribunals::Application.routes.draw do
-  get '/utiac/decisions', to: 'decisions#index', as: :root
-  get '/aac/decisions', to: 'aac_decisions#index'
-  get '/eat/decisions', to: 'eat_decisions#index'
-  get '/tax/decisions', to: 'ftt_decisions#index'
+  get '/', to: redirect('/utiac')
+  get '/utiac/decisions', to:  redirect('/utiac')
+  get '/utiac', to: 'decisions#index', as: :root
+
+  get '/utaac', to: 'aac_decisions#index'
+  get '/eat', to: 'eat_decisions#index'
+  get '/ftt-tax', to: 'ftt_decisions#index'
 
   scope '/utiac' do
-    resources :decisions
-    get '/' => redirect('/utiac/decisions')
+    resources :decisions, path: ''
+    get '/decisions/:id', to: redirect('/utiac/%{id}')
   end
 
-  scope '/aac' do
-    resources :aac_decisions
+  scope '/utaac' do
+    resources :aac_decisions, path: ''
   end
 
   scope '/eat' do
-    resources :eat_decisions
+    resources :eat_decisions, path: ''
   end
 
-  scope '/tax' do
-    resources :ftt_decisions
+  scope '/ftt-tax' do
+    resources :ftt_decisions, path: ''
   end
 
   namespace :admin do
-    resources :decisions
+    #TODO: Temporarily redirecting to UTIAC, but later on admins should be redirected to their respective tribunal's admin panel.
+    get '/', to: redirect('/admin/utiac')
+    scope '/utiac' do    
+      resources :decisions, path: ''
+    end
     resource :authentication do
       get :logout
     end
   end
-  get '/' => redirect('/utiac/decisions')
 
   resource :feedback
 end
