617cb3d5cc13ecd5dd69514b87218f855cc0cb12
Danny Hawkins <danny.hawkins@gmail.com>
2014-03-19 17:37:32 +0000

    added tribunal object

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,15 @@
 class User < ActiveRecord::Base
   devise :invitable, :database_authenticatable, :recoverable, :rememberable, :trackable, :validatable
+  # associations
+  has_and_belongs_to_many :tribunals
+  belongs_to :primary_tribunal
+  # callbacks
+  before_save :assign_primary_tribunal
 
+
+  protected
+
+  def assign_primary_tribunal
+    self.primary_tribunal ||= tribunals.first if tribunals.count > 0
+  end
 end
