855f3dac9f8f2e2ee205a0a7f716e0232959c540
Ikenna Okpala <ikenna.okpala@digital.justice.gov.uk>
2014-03-19 16:46:05 +0000

    admin edit index and show templates and admin ftt decision's controller

---
+++ app/controllers/admin/ftt_decisions_controller.rb
@@ -0,0 +1,57 @@
+class Admin::FttDecisionsController < Admin::DecisionsController
+
+  def index
+    set_cache_control(FttDecision.maximum(:updated_at))
+    @order_by = 'decision_date'
+    @sort_options = [["Date of decision", "decision_date"], ["Date of update", "last_updatedtime"]]
+    params[:search] ||= {}
+    if params[:search].present?
+       @order_by = params[:search][:sort] || @order_by
+    end
+    @date_column_title = (@order_by == 'decision_date') ? 'Date of decision' : 'Date of update'
+    @categories_title = 'Categories: '
+    @decisions = FttDecision.ordered.paginate(:page => params[:page], :per_page => 30)
+    @decisions = @decisions.filtered(params[:search]) if params[:search].present?
+  end
+
+  def show
+    @decision = FttDecision.find(params[:id])
+    set_cache_control(@decision.updated_at)
+  end
+
+  def create
+    @decision = FttDecision.new(decision_params)
+    if @decision.save
+      @decision.process_doc
+      redirect_to admin_ftt_decision_path
+    else
+      render new_admin_ftt_decision_path
+    end
+  end
+
+  def new
+    @decision ||= FttDecision.new
+  end
+
+  def edit
+    @decision = self.class.scope.find(params[:id])
+  end
+
+  def update
+    @decision = self.class.scope.find(params[:id])
+    @decision.update_attributes!(decision_params)
+    redirect_to admin_ftt_decision_path
+  rescue
+    redirect_to edit_admin_ftt_decision_path(@decisions)
+  end
+
+  def destroy
+    @decision = self.class.scope.find(params[:id])
+    @decision.destroy
+    redirect_to admin_decisions_path
+  end
+
+  def self.scope
+    FttDecision.all
+  end
+end
