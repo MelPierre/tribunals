45c328cd186cf40913640f2782c0f78d7081925f
Danny Hawkins <danny.hawkins@gmail.com>
2014-03-20 14:18:15 +0000

    changes to primary tribunal

--- db/migrate/20140319173003_add_primary_tribunal_id_to_users.rb
+++
@@ -1,6 +0,0 @@
-class AddPrimaryTribunalIdToUsers < ActiveRecord::Migration
-  def change
-    add_column :users, :primary_tribunal_id, :integer
-    add_index :users, :primary_tribunal_id
-  end
-end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -2,45 +2,7 @@ require 'spec_helper'
 
 describe User do
   it{ should have_and_belong_to_many(:tribunals) }
-  it{ should belong_to(:primary_tribunal) }
   it{ should validate_presence_of(:email) }
-
-  describe '#save' do
-
-    it 'should call assign_primary_tribunal' do
-      user = build(:user)
-      expect(user).to receive(:assign_primary_tribunal)
-      user.save
-    end
-
-  end
-
-  describe '#assign_primary_tribunal' do
-    let(:user) { create(:user) }
-
-    it 'assigns nothing without a tribunal' do
-      user.send(:assign_primary_tribunal)
-      user.primary_tribunal.should be_nil
-    end
-
-    it 'assigns the first tribunal if one is not assigned' do
-      tribunal = create(:tribunal)
-      user.tribunals << tribunal
-
-      user.send(:assign_primary_tribunal)
-      user.primary_tribunal.should eq(tribunal)
-    end
-
-    it 'it retains the existing tribunal if one is assigned' do
-      primary_tribunal = build(:tribunal)
-      
-      user.tribunals << build(:tribunal)
-      user.primary_tribunal = primary_tribunal
-
-      user.send(:assign_primary_tribunal)
-      user.primary_tribunal.should eq(primary_tribunal)
-    end
-  end
 
   describe '#has_tribunal?' do
     let(:user) { build(:user) }
@@ -68,21 +30,5 @@ describe User do
 
     end
 
-    context 'with aac as primary tribunal' do
-      before do
-        user.primary_tribunal = build(:tribunal, code: 'aac')
-      end
-
-      it 'returns true when passed aac as string or symbol' do
-        user.has_tribunal?('aac').should be_true
-        user.has_tribunal?(:aac).should be_true
-      end
-
-      it 'returns false when passed ftt as string or symbol' do
-        user.has_tribunal?('ftt').should be_false
-        user.has_tribunal?(:ftt).should be_false
-      end
-
-    end
   end
 end
