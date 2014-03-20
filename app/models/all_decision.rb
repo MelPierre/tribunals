606da7a0f477915184be0037dc8a178bb9024bbb
Hrishi Mittal <hrishimittal@gmail.com>
2014-03-20 14:19:13 +0000

    Create new model AllDecisions. WIP.

---
+++ app/models/all_decision.rb
@@ -0,0 +1,6 @@
+class AllDecision < ActiveRecord::Base
+  # has_many :subcategories_decisions
+  # has_many :subcategories, through: :subcategories_decisions
+  # has_many :judgments
+  # has_many :judges, through: :judgements
+end
