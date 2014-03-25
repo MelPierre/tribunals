fa16c4970a79f33bb3f0f157ff37a2c1bf527789
Danny Hawkins <danny.hawkins@gmail.com>
2014-03-25 17:20:35 +0000

    updated from comments

--- app/controllers/users/invitations_controller.rb
+++ app/controllers/users/invitations_controller.rb
@@ -1,5 +1,5 @@
 class Users::InvitationsController < Devise::InvitationsController
-  before_filter :require_admin!, only: [:new]
+  before_filter :require_admin!, only: [:new, :destroy]
 
   def invite_params
     params.require(:user).permit(:email, :admin, tribunal_ids: [])
