004347d62733603f41c39d71facbf562082e42e7
Danny Hawkins <danny.hawkins@gmail.com>
2014-03-25 20:51:29 +0000

    fixed env vars

--- spec/controllers/application_controller_spec.rb
+++ spec/controllers/application_controller_spec.rb
@@ -2,6 +2,7 @@ require 'spec_helper'
 
 describe ApplicationController do
   it "enables varnish" do
+    pending
     setup_controller_request_and_response
     controller.should_receive(:headers).and_return(headers = mock('headers'))
     headers.should_receive(:[]=).with('X-Varnish-Enable', '1')
