e1a126157444bdc73445d7751b5a871f492072c7
Danny Hawkins <danny.hawkins@gmail.com>
2014-03-26 15:36:57 +0000

    moved env setup

--- config/initializers/_env.rb
+++ config/initializers/_env.rb
@@ -1,10 +1,1 @@
 ENV['PATH'] += ':/opt/libreoffice4.0/program/'
-
-# This should load from env, these are for defaults
-ENV['DEVISE_SECRET'] ||= '12341241243124124124124124124124'
-ENV['DEVISE_SENDER'] ||= 'no-reply@tribunalsdecisions.service.gov.uk'
-
-ENV['SMTP_HOST']      ||= 'tribunalsdecisions.service.gov.uk'
-ENV['SMTP_HOSTNAME']  ||= "localhost"
-ENV['SMTP_USERNAME']  ||= ""
-ENV['SMTP_PASSWORD']  ||= ""
