c8e434b2348fb6c1d9cc071803e2999df800f867
Danny Hawkins <danny.hawkins@gmail.com>
2014-04-01 17:03:11 +0100

    work on import scripts

--- lib/csv_importer.rb
+++ lib/csv_importer.rb
@@ -1,9 +1,10 @@
 require 'csv'
 
 class CSVImporter
-  def initialize(directory, logger=Rails.logger)
+  def initialize(directory, code, logger=Rails.logger)
     @directory = directory
     @logger = logger
+    @tribunal = Tribunal.find_by_code(code)
   end
   
   def each_row(fn, &blk)
@@ -15,35 +16,34 @@ class CSVImporter
   end
 
   def import_decisions
-    AacDecision.delete_all    
+    @tribunal.all_decisions.delete_all    
     each_row('judgements.csv') do |row|
       create_decision(row)
     end
   end
 
   def import_categories
-    AacDecisionCategory.delete_all
+    @tribunal.categories.delete_all
     each_row('categories.csv') do |row|
       create_category(row)
     end
   end
 
   def import_subcategories
-    AacDecisionSubcategory.delete_all
+    @tribunal.subcategories.delete_all
     each_row('subcategories.csv') do |row|
       create_subcategory(row)
     end
   end
 
   def import_judges
-    Judge.delete_all
+    @tribunal.all_judges.delete_all
     each_row('judges.csv') do |row|
       create_judge(row)
     end
   end
 
-  def import_decisions_judges_mapping
-    AacJudgement.delete_all    
+  def import_decisions_judges_mapping  
     each_row('judges_judgements_map.csv') do |row|
       create_aac_judgements(row)
     end    
@@ -60,7 +60,7 @@ class CSVImporter
   end
 
   def find_decision(filename, ncn)
-    Decision.where('doc_file = ? or ncn = ?', filename, ncn).first
+    @tribunal.all_decisions.where('doc_file = ? or ncn = ?', filename, ncn).first
   end
 
   def compute_ncn(row)
@@ -112,52 +112,52 @@ class CSVImporter
   end
 
   def create_decision(row)
-    begin
-      d = AacDecision.new(id: row['judgement_id'],
-                       tribunal: row['tribunal'],
-                       chamber: row['chamber'],
-                       chamber_group: row['chamber_group'],
-                       hearing_date: read_date(row['hearing_datetime'],'%m/%d/%Y'),
-                       decision_date: read_date(row['decision_datetime'],'%m/%d/%Y'),
-                       created_datetime: read_date(row['created_datetime'],'%m/%d/%Y'),
-                       publication_date: read_date(row['publication_datetime'],'%m/%d/%Y'),
-                       last_updatedtime: read_date(row['last_updatedtime'],'%m/%d/%Y'),
-                       file_number: row['file_number'],
-                       file_no_1: row['file_no_1'],
-                       file_no_2: row['file_no_2'],
-                       file_no_3: row['file_no_3'],
-                       reported_number: row['reported_number'],
-                       reported_no_1: row['reported_no_1'],
-                       reported_no_2: row['reported_no_2'],
-                       reported_no_3: row['reported_no_3'],
-                       ncn: row['neutral_citation_number'],
-                       ncn_year: row['ncn_year'],
-                       ncn_code1: row['ncn_code1'],
-                       ncn_citation: row['ncn_citation'],
-                       ncn_code2: row['ncn_code2'],
-                       claimant: row['claimant'],
-                       respondent: row['respondent'],
-                       notes: row['notes'],
-                       is_published: row['is_published'],
-                       aac_decision_subcategory_id: row['subcategory_id'],
-                       old_sec_subcategory_id: row['sec_subcategory_id'],
-                       keywords: row['keywords']
-                       )
-      d.save
-    rescue StandardError => e
-      puts e
-      puts "Failed to import #{row['judgement_id']}"
-    end
+    # begin
+    #   d = AacDecision.new(id: row['judgement_id'],
+    #                    tribunal: row['tribunal'],
+    #                    chamber: row['chamber'],
+    #                    chamber_group: row['chamber_group'],
+    #                    hearing_date: read_date(row['hearing_datetime'],'%m/%d/%Y'),
+    #                    decision_date: read_date(row['decision_datetime'],'%m/%d/%Y'),
+    #                    created_datetime: read_date(row['created_datetime'],'%m/%d/%Y'),
+    #                    publication_date: read_date(row['publication_datetime'],'%m/%d/%Y'),
+    #                    last_updatedtime: read_date(row['last_updatedtime'],'%m/%d/%Y'),
+    #                    file_number: row['file_number'],
+    #                    file_no_1: row['file_no_1'],
+    #                    file_no_2: row['file_no_2'],
+    #                    file_no_3: row['file_no_3'],
+    #                    reported_number: row['reported_number'],
+    #                    reported_no_1: row['reported_no_1'],
+    #                    reported_no_2: row['reported_no_2'],
+    #                    reported_no_3: row['reported_no_3'],
+    #                    ncn: row['neutral_citation_number'],
+    #                    ncn_year: row['ncn_year'],
+    #                    ncn_code1: row['ncn_code1'],
+    #                    ncn_citation: row['ncn_citation'],
+    #                    ncn_code2: row['ncn_code2'],
+    #                    claimant: row['claimant'],
+    #                    respondent: row['respondent'],
+    #                    notes: row['notes'],
+    #                    is_published: row['is_published'],
+    #                    aac_decision_subcategory_id: row['subcategory_id'],
+    #                    old_sec_subcategory_id: row['sec_subcategory_id'],
+    #                    keywords: row['keywords']
+    #                    )
+    #   d.save
+    # rescue StandardError => e
+    #   puts e
+    #   puts "Failed to import #{row['judgement_id']}"
+    # end
   end
 
   def create_category(row)
-    c = AacDecisionCategory.new(id: row['category_id'], name: row['category_name'])
+    c = @tribunal.categories.new(legacy_id: row['category_id'], name: row['category_name'])
     puts "Failed to import #{row['category_id']} - #{row['category_name']}" unless c.save
   end
 
   def create_subcategory(row)
-    if c = AacDecisionCategory.find(row['category_id'])
-      sc = c.aac_decision_subcategories.new(id: row['subcategory_id'], name: row['subcategory_name'])
+    if c = @tribunal.categories.find_by_legacy_id(row['category_id'])
+      sc = c.aac_decision_subcategories.new(legacy_id: row['subcategory_id'], name: row['subcategory_name'])
       puts "Failed to import #{row['subcategory_id']} - #{row['subcategory_name']}" unless sc.save
     else
       puts "Could not find category with id #{row['category_id']}"
@@ -165,18 +165,18 @@ class CSVImporter
   end
 
   def create_judge(row)
-    j = Judge.new(id: row['judge_id'], name: row['judge_name'])
+    j = @tribunal.all_judges.new(legacy_id: row['judge_id'], name: row['judge_name'])
     puts "Failed to import #{row['judge_id']} - #{row['judge_name']}" unless j.save    
   end    
 
   def create_aac_judgements(row)
-    begin
-      d = AacDecision.find(row['judgement_id'])
-      d.judges << Judge.find(row['judge_id'].to_i)
-    rescue Exception => e
-      puts e.message  
-      puts e.backtrace.inspect  
-    end
+    # begin
+    #   d = AacDecision.find(row['judgement_id'])
+    #   d.judges << Judge.find(row['judge_id'].to_i)
+    # rescue Exception => e
+    #   puts e.message  
+    #   puts e.backtrace.inspect  
+    # end
   end
 
   def judges_for(judgment_id)
