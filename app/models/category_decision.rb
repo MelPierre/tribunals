f774d3cfdc7e5b26dceff6da8dbd0b0a1fdf295c
Hrishi Mittal <hrishimittal@gmail.com>
2014-04-03 16:07:47 +0100

    MOAR SPECS

--- app/models/category_decision.rb
+++ app/models/category_decision.rb
@@ -2,4 +2,10 @@ class CategoryDecision < ActiveRecord::Base
   belongs_to :all_decision
   belongs_to :subcategory
   belongs_to :category
+
+  before_save :assign_category
+
+  def assign_category
+    self.category_id ||= Subcategory.find_by_id(self.subcategory_id).try(:category).try(:id)
+  end
 end
--- spec/models/all_decision_spec.rb
+++ spec/models/all_decision_spec.rb
@@ -2,22 +2,21 @@ require 'spec_helper'
 
 describe AllDecision do
   describe "search" do
-    let!(:decision1) { create(:all_decision, all_decision_hash(text: "Some searchable green text is here")) }
-    let!(:decision2) { create(:all_decision, all_decision_hash(text: "beautiful searchable text is here gerald")) }
+    let!(:utaac) { create(:tribunal, code: "utaac", title: "Upper Tribunal Administrative Appeals Chamber") }
+    let!(:decision1) { create(:all_decision, all_decision_hash(tribunal_id: utaac.id, text: "Some searchable green text is here")) }
+    let!(:decision2) { create(:all_decision, all_decision_hash(tribunal_id: utaac.id, text: "beautiful searchable text is here gerald")) }
     let!(:judge1) { create(:all_judge, {name: "Blake"}) }
-    let!(:decision3) { create(:all_decision, all_decision_hash(claimant: "gerald", text:"Some beautiful decision made long ago",
+    let!(:decision3) { create(:all_decision, all_decision_hash(tribunal_id: utaac.id, claimant: "gerald", text:"Some beautiful decision made long ago",
                                                                 all_judge_ids: [judge1.id])) }
-    # let(:adc) { AacCategory.create!(name: "Benefits for children") }
-    # let(:adsc) { AacSubcategory.create!(name: "Children's Income", aac_category_id: adc.id) }
+    let!(:adc) { create(:category, name: "Benefits for children") }
+    let!(:adsc) { create(:subcategory, name: "Children's Income", category_id: adc.id) }
+    let!(:adsc2) { create(:subcategory, name: "Free Education", category_id: adc.id) }
 
-    let!(:decision4) { create(:all_decision, all_decision_hash(claimant: 'Green')) }
-    # decision4.aac_subcategories << adsc
-    # decision4.save!
+    let!(:decision4) { create(:all_decision, all_decision_hash(tribunal_id: utaac.id, claimant: 'Green', 
+                                          subcategory_ids: [adsc.id])) }
 
-    # let(:decision5) { create(:all_decision, all_decision_hash(ncn: '[2013] UKUT 456', text: 'little pete was a green boy', 
-                                          # aac_decision_subcategory_id: adsc.id)) }
-    # decision5.aac_subcategories << adsc
-    # decision5.save!
+    let!(:decision5) { create(:all_decision, all_decision_hash(tribunal_id: utaac.id, neutral_citation_number: '[2013] UKUT 456', text: 'little pete was a green boy', 
+                                          subcategory_ids: [adsc2.id])) }
 
     it "should filter on search text" do
       AllDecision.filtered(query: "beautiful").should == [decision2, decision3]
@@ -31,18 +30,23 @@ describe AllDecision do
       AllDecision.filtered(query: "gerald", judge: 'Blake').should == [decision3]
     end
 
-
-    pending "should filter on category" do
-      AllDecision.filtered(:category => "Benefits for children").sort.should == [decision4, decision5]
+    it "should filter on category" do
+      results = AllDecision.filtered(:category => "Benefits for children").sort
+      results.count.should == 2
+      results.should == [decision4, decision5]
     end
 
+    it "should filter on subcategory" do
+      results = AllDecision.filtered(:subcategory => "Free Education").sort
+      results.count.should == 1
+      results.should == [decision5]
+    end
+
+    ##############################################
     pending "should filter on search text and category" do
       AllDecision.filtered(:query => "green", :category => "Benefits for children").sort.should == [decision4, decision5]
     end
 
-    pending "should filter on subcategory" do
-      AllDecision.filtered(:subcategory => "Children's Income").sort.should == [decision4, decision5]
-    end
 
     pending "should filter on search text and subcategory" do
       AllDecision.filtered(:query => "[2013] UKUT 456", :subcategory => "Children's Income").should == [decision5]
