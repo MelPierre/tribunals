5acf40cea8dae3a9a14db260ddce63e645e646db
Ikenna Okpala <ikenna.okpala@digital.justice.gov.uk>
2014-04-03 17:13:13 +0100

    Use consistent values

--- spec/features/admin/forms/ftt_tax_feature_spec.rb
+++ spec/features/admin/forms/ftt_tax_feature_spec.rb
@@ -6,10 +6,10 @@ feature 'First Tier Tribunal Tax Chamber' do
 
   context 'as standard user' do
     before do
-      create(:all_judge, tribunal_id: tribunal.id)
-      create(:all_judge, tribunal_id: tribunal.id)
-      create(:category, tribunal_id: tribunal.id)
-      create(:subcategory, tribunal_id: tribunal.id)
+      create(:all_judge, name: 'Iker Casillas', tribunal_id: tribunal.id)
+      create(:all_judge, name: 'Rafael Nadal', tribunal_id: tribunal.id)
+      create(:category, name: 'VAT - Taxes' , tribunal_id: tribunal.id)
+      create(:subcategory, name:  'VAT - Taxes - Monthly', tribunal_id: tribunal.id)
       visit '/admin'
       sign_in user
     end
@@ -25,12 +25,12 @@ feature 'First Tier Tribunal Tax Chamber' do
       fill_in('Decision No', with: '987789')
       fill_in('Appellant Name', with: 'Jonh Smith')
       fill_in('Respondent Name', with: 'Matt Black')
-      select('Justice Judge 1', from: 'Judge Name')
+      select('Rafael Nadal', from: 'Judge Name')
       fill_in('Decision date', with: '21/01/1980')
       fill_in('Date of Upload', with: '31/01/1978')
       fill_in('Date published', with: '14/02/1967')
-      select('Category 1', from: 'Category')
-      select('SubCategory 1', from: 'Sub-Category')
+      select('VAT - Taxes', from: 'Category')
+      select('VAT - Taxes - Monthly', from: 'Sub-Category')
       fill_in('Notes', with: 'filling the notes for testing')
 
       click_button('Add decision')
@@ -40,14 +40,13 @@ feature 'First Tier Tribunal Tax Chamber' do
       expect(page).to have_content('Decision Number: 987789')
       expect(page).to have_content('Appellant name: Jonh Smith')
       expect(page).to have_content('Respondent name: Matt Black')
-      expect(page).to have_content('Judges: Justice Judge 1')
+      expect(page).to have_content('Judges: Rafael Nadal')
       expect(page).to have_content('Date of decision: 21 Jan 1980')
       expect(page).to have_content('Date added: 31 Jan 1978')
       expect(page).to have_content('Date updated: 14 Feb 1967')
-      expect(page).to have_content('Category: Category 1')
-      expect(page).to have_content('SubCategories: SubCategory 1')
+      expect(page).to have_content('Category: VAT - Taxes')
+      expect(page).to have_content('SubCategories: VAT - Taxes - Monthly')
       expect(page).to have_content('Notes filling the notes for testing')
-
     end
 
     # scenario 'list all available ftt decision' do
