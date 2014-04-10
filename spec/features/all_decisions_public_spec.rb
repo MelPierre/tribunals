ed42a6f0d8668fbb0555de14f61a3408f3b23bc1
David Hernandez <david.hernandez@digital.justice.gov.uk>
2014-04-10 13:54:25 +0100

    insert only one decision instead of 30, that is not necessary for this test

--- spec/features/all_decisions_public_spec.rb
+++ spec/features/all_decisions_public_spec.rb
@@ -6,7 +6,7 @@ require 'spec_helper'
 
     context 'FTT' do
       scenario "can view the index page of FTT tribunal" do
-        30.times{ create(:all_decision, all_decision_hash(tribunal_id: ftt.id, file_number: 'FTT24267', text: "Some searchable green text is here")) }
+        create(:all_decision, all_decision_hash(tribunal_id: ftt.id, file_number: 'FTT24267', text: "Some searchable green text is here"))
         visit '/ftt-tax'
         expect(page).to have_content('My Holy Tribunal of Very Fine Tax Justice For All')
         expect(page).to have_content('FTT24267')
@@ -21,7 +21,7 @@ require 'spec_helper'
 
     context 'EAT' do
       scenario "can view the index page of EAT tribunal" do
-        30.times{ create(:all_decision, all_decision_hash(tribunal_id: eat.id, file_number: 'EAT24267', text: "Some searchable green text is here")) }
+        create(:all_decision, all_decision_hash(tribunal_id: eat.id, file_number: 'EAT24267', text: "Some searchable green text is here"))
         visit '/eat'
         expect(page).to have_content('My Holy Tribunal of Very Fine EAT Justice For All')
         expect(page).to have_content('EAT24267')
