c77b47a0f4762346204e5230dff39dc5875fcd3e
Danny Hawkins <danny.hawkins@gmail.com>
2014-04-10 14:58:54 +0100

    updated routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -5,14 +5,8 @@ Tribunals::Application.routes.draw do
   #UTIAC to be refactored into all_decisions routes after the 3 new tribunals are done and tested
   get '/utiac/decisions/:id', to:  redirect{|params, request| "/utiac/#{params[:id]}" }
   get '/utiac/decisions', to:  redirect('/utiac')
+  
   resources :decisions, path: 'utiac'
-
-<<<<<<< HEAD
-  resources :all_decisions, path: 'all'
-  # scope ':tribunal_code' do
-  #   resources :all_decisions, path: ''
-  # end
-=======
 
   resources :all_decisions, path: 'ftt-tax', tribunal_code: 'ftt-tax'
   resources :all_decisions, path: 'eat', tribunal_code: 'eat'
@@ -22,7 +16,7 @@ Tribunals::Application.routes.draw do
   scope ':tribunal_code' do
     # resources :all_decisions, path: '', tribunal_code: :tribunal_code
   end
->>>>>>> develop
+
 
   # TODO: These redirect parts seem messy and maybe not the correct way to manage the requirement, need to review
   namespace :admin do
