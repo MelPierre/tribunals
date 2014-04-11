c697dc73817c1d7256df09d9b04e806f8394017a
Danny Hawkins <danny.hawkins@gmail.com>
2014-04-11 17:22:18 +0100

    categories blank index working;

--- spec/features/admin/manage_ftt_categories_feature_spec.rb
+++ spec/features/admin/manage_ftt_categories_feature_spec.rb
@@ -1,15 +1,30 @@
 require 'spec_helper'
 
 feature 'Manage FTT categories' do
+  let!(:ftt) { create(:tribunal, name: 'ftt-tax', code: 'ftt-tax') }
+  let!(:admin) { create(:user, admin: true) }
 
-  context 'cannot view page without authentication' do
+  context 'without authentication' do
 
+    scenario 'cannot view ftt categories index' do
+      visit '/admin/ftt-tax/categories'
+
+      expect(page).to have_content('You need to sign in or sign up before continuing')
+    end
+  
   end
 
-  context 'as an admin' do
+  context 'as admin' do
+
+    background do
+      visit '/admin'
+      sign_in admin
+    end
 
     scenario 'view the ftt categories index page' do
+      visit '/admin/ftt-tax/categories'
 
+      expect(page).to have_content('Categories')
     end
 
     scenario 'view paginated list of categories' do
