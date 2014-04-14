6f0b2052079f28399207c00a59b50c19b96ed59d
Ikenna Okpala <ikenna.okpala@digital.justice.gov.uk>
2014-04-14 12:46:54 +0100

    Define test to check that all the prerequisites for all task are present

--- spec/lib/tasks/import_rake_spec.rb
+++ spec/lib/tasks/import_rake_spec.rb
@@ -1,6 +1,16 @@
 require "spec_helper"
 require "rake"
 
+
+
+describe "import:eat:all" do
+  include_context "rake"
+  its(:prerequisites) { should include("categories")}
+  its(:prerequisites) { should include("subcategories")}
+  its(:prerequisites) { should include("decisions_judges")}
+  its(:prerequisites) { should include("map_categories_to_decisions")}
+end
+
 describe "import:eat:categories" do
   include_context "rake"
   its(:prerequisites) { should include("environment")}
