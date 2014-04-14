71ac6b0a84f2e1b34a1f558fee13f2a730baabe7
Ikenna Okpala <ikenna.okpala@digital.justice.gov.uk>
2014-04-14 15:49:27 +0100

    Add tests for import:eat:categories

--- spec/lib/tasks/import_rake_spec.rb
+++ spec/lib/tasks/import_rake_spec.rb
@@ -1,8 +1,6 @@
 require "spec_helper"
 require "rake"
 
-
-
 describe "import:eat:all" do
   include_context "rake"
   its(:prerequisites) { should include("categories")}
@@ -13,5 +11,24 @@ end
 
 describe "import:eat:categories" do
   include_context "rake"
+  let(:import_categories) {double('importing categories to database')}
+  let(:init) {double('intializing CSVImporter', import_categories: import_categories)}
+
   its(:prerequisites) { should include("environment")}
+  context 'rake task' do
+
+    before do
+      CSVImporter.stub(:new => init)
+      Object.stub(:eat)
+      subject.invoke
+    end
+
+    it "creates CSVImporter with correct eat csv directory and code" do
+      CSVImporter.should have_received(:new).with('data/eat', 'eat')
+    end
+
+    it "can use import_categories on CSVImporter" do
+      CSVImporter.new.should have_received(:import_categories)
+    end
+  end
 end
