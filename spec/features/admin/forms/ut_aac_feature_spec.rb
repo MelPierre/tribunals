133e915db376fa98e06e8d9efd249259224c6f6c
Jairo Diaz <jairo.diaz@codescrum.com>
2014-04-14 08:21:46 +0100

    Rebased with develop and corrected aac code

--- spec/features/admin/forms/ut_aac_feature_spec.rb
+++ spec/features/admin/forms/ut_aac_feature_spec.rb
@@ -1,7 +1,7 @@
 require 'spec_helper'
 
 feature 'Administrative appeals chamber: decisions on appeals to the Upper Tribunal' do
-  let(:tribunal){ create(:tribunal, code: 'utacc', title: 'Administrative appeals chamber: decisions on appeals to the Upper Tribunal')}
+  let(:tribunal){ create(:tribunal, code: 'utaac', title: 'Administrative appeals chamber: decisions on appeals to the Upper Tribunal')}
   let(:user) { create(:user, tribunals: [tribunal]) }
 
   context 'as standard user' do
