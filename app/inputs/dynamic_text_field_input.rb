b6f219c87eb67f257da02d45dd5ecde7ca9c1fd9
Ikenna Okpala <ikenna.okpala@digital.justice.gov.uk>
2014-04-21 09:07:28 +0100

    Adjust styling

--- app/inputs/dynamic_text_field_input.rb
+++ app/inputs/dynamic_text_field_input.rb
@@ -1,7 +1,7 @@
 class DynamicTextFieldInput < SimpleForm::Inputs::Base
 
   def input
-    template.content_tag(:span, class: "input string optional #{attribute_name}", style: "margin-left:107px") do
+    template.content_tag(:span, class: "input string optional #{attribute_name}", style: "margin-left:45px") do
       template.concat @builder.text_field(attribute_name, input_html_options(0))
       template.concat link_text
     end
@@ -12,7 +12,7 @@ class DynamicTextFieldInput < SimpleForm::Inputs::Base
     def input_html_options(position)
       model = @builder.object_name
       name = "#{model}[#{column.name}][]"
-      {id: "#{model}_#{position}", name: "#{name}", style:'margin:5px;'}
+      {id: "#{model}_#{position}", name: "#{name}", style:'width:390px;margin:5px;'}
     end
 
     def link_text
