9d4c0965b48024e6d0c0634f81424c673b3d7d8a
Ikenna Okpala <ikenna.okpala@digital.justice.gov.uk>
2014-04-22 12:25:47 +0100

    Add model and column names to ncn input

--- app/inputs/dynamic_text_field_input.rb
+++ app/inputs/dynamic_text_field_input.rb
@@ -1,7 +1,7 @@
 class DynamicTextFieldInput < SimpleForm::Inputs::Base
 
   def input
-    template.content_tag(:span, class: "input string optional #{attribute_name}", style: "margin-left:45px") do
+    template.content_tag(:span, class: "input string optional #{attribute_name}", style: "width:120px;margin-left:45px;") do
       template.concat @builder.text_field(attribute_name, input_html_options(0))
       template.concat link_text
     end
@@ -12,10 +12,10 @@ class DynamicTextFieldInput < SimpleForm::Inputs::Base
     def input_html_options(position)
       model = @builder.object_name
       name = "#{model}[#{column.name}][]"
-      {id: "#{model}_#{position}", name: "#{name}", style:'width:390px;margin:5px;'}
+      {id: "#{model}_#{position}", name: "#{name}", style:'width:390px;margin:5px; display:inline-block;'}
     end
 
     def link_text
-      "<a href='#{}' id=add_#{column.name}> Add another #{column.name}</a>".html_safe
+      "<a href='#{}' id=add_#{column.name} class=add_text_field> Add another #{column.name}</a>".html_safe
     end
 end
--- app/inputs/ncn_input.rb
+++ app/inputs/ncn_input.rb
@@ -2,7 +2,7 @@ class NcnInput < SimpleForm::Inputs::Base
 
   def input
     template.content_tag(:span, class: "input string optional #{attribute_name}", style: "margin-left:125px") do
-      template.concat @builder.select(attribute_name, year_range)
+      template.concat @builder.select(attribute_name, year_range, input_html_options)
       template.concat @builder.text_field(attribute_name, input_html_options.merge({value: 'UKUT'}))
       template.concat @builder.text_field(attribute_name, number_field_html_options)
       template.concat @builder.text_field(attribute_name, input_html_options.merge({value: 'AAC'}))
@@ -20,7 +20,9 @@ class NcnInput < SimpleForm::Inputs::Base
     end
 
     def input_html_options
-      {id: 'ncn', size: '5px', style:'width:50px;margin:5px;min-width:0'}
+      model = @builder.object_name
+      name = "#{model}[#{column.name}][]"
+      {name: "#{name}", size: '5px', style:'width:50px;margin:5px;min-width:0'}
     end
 
     def number_field_html_options
